7b6e02c76a315b3ac1bfc7e93eb510e2d05761fc
Yoji Yamamoto <ajfrlc9808@gmail.com>
2022-10-15 23:46:06 +0900

    [update] refactoring name

--- app/src/main/java/com/yoji0806/paperineconomics/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/yoji0806/paperineconomics/ui/bookmark/BookmarkFragment.kt
@@ -1,4 +1,4 @@
-package com.yoji0806.paperineconomics.ui.gallery
+package com.yoji0806.paperineconomics.ui.bookmark
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.yoji0806.paperineconomics.databinding.FragmentGalleryBinding
 
-class GalleryFragment : Fragment() {
+class BookmarkFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
 
@@ -23,7 +23,7 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(BookmarkViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/com/yoji0806/paperineconomics/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/yoji0806/paperineconomics/ui/bookmark/BookmarkViewModel.kt
@@ -1,10 +1,10 @@
-package com.yoji0806.paperineconomics.ui.gallery
+package com.yoji0806.paperineconomics.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class BookmarkViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
